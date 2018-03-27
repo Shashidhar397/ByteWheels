d5188da459ae4d459b81f951642c374a51584f57
sppraveen <praveen.sp@foodlinked.com>
2018-03-28 00:29:15 +0530

    DB Transaction configuration

--- ByteWheels/ByteWheelsJavaService/src/main/java/com/bytewheels/app/services/ByteWheelsServiceImpl.java
+++ ByteWheels/ByteWheelsJavaService/src/main/java/com/bytewheels/app/services/ByteWheelsServiceImpl.java
@@ -1,15 +1,25 @@
 package com.bytewheels.app.services;
 
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.bytewheels.app.dao.VehicleDaoIF;
 
 @Service
+@Transactional
 public class ByteWheelsServiceImpl implements ByteWheelsServiceIF{
 	
 	private static final Logger logger = Logger.getLogger(ByteWheelsServiceImpl.class);
 	
+	@Autowired
+	private VehicleDaoIF vehicleDao;
+	
+	
 	public String process() {
 		logger.info("Hello World is working");
+		vehicleDao.persistVehicle();
 		return "Hello World";
 	}
 
